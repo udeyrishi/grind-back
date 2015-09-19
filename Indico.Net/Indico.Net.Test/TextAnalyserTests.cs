5e14612a402d03448572072149b9726b820204fd
Udey Rishi <udey.rishi@gmail.com>
2015-09-19 03:44:50 -0400

    Added HQ sentiment analysis + text tags

--- Indico.Net/Indico.Net.Test/TextAnalyserTests.cs
+++ Indico.Net/Indico.Net.Test/TextAnalyserTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Indico.Net.Test
 {
@@ -31,6 +32,45 @@ namespace Indico.Net.Test
             double[] result = await analyser.AnalyseSentimentAsync(new[] { "This is bad.", "This is good." } );
             result[0].Should().BeLessThan(0.1);
             result[1].Should().BeGreaterThan(0.9);
+        }
+
+        [TestMethod]
+        public async Task AnalyseSentimentHq_PositiveString_Works()
+        {
+            double result = await analyser.AnalyseSentimentHighQualityAsync("This is good.");
+            result.Should().BeGreaterThan(0.9);
+        }
+
+        [TestMethod]
+        public async Task AnalyseSentimentHq_NegativeString_Works()
+        {
+            double result = await analyser.AnalyseSentimentHighQualityAsync("This is bad.");
+            result.Should().BeLessThan(0.1);
+        }
+
+
+        [TestMethod]
+        public async Task AnalyseSentimentHq_Batch_Works()
+        {
+            double[] result = await analyser.AnalyseSentimentHighQualityAsync(new[] { "This is bad.", "This is good." });
+            result[0].Should().BeLessThan(0.1);
+            result[1].Should().BeGreaterThan(0.9);
+        }
+
+        [TestMethod]
+        public async Task TextTags_Works()
+        {
+            Dictionary<string, double> result = await analyser.GetTextTagsAsync("This is bad.");
+            result.Count.Should().BeGreaterThan(0);
+        }
+
+
+        [TestMethod]
+        public async Task TextTagsBatch_Works()
+        {
+            Dictionary<string, double>[] result = await analyser.GetTextTagsAsync(new string[] { "This is bad.", "This is good." });
+            result[0].Count.Should().BeGreaterThan(0);
+            result[1].Count.Should().BeGreaterThan(0);
         }
     }
 }
