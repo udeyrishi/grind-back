db7832efb941b747ebe3b05df854a7bfb5003ce9
Udey Rishi <udey.rishi@gmail.com>
2015-09-19 13:44:27 -0400

    Updated WebhoseClient to use pre-existing webhose libraries

--- Grind.Core/Grind.Core/Webhose/WebhoseClient.cs
+++ Grind.Core/Grind.Core/Webhose/WebhoseClient.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using webhose;
-using Grind.Core;
+﻿using System.Threading.Tasks;
 using Utilities;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using webhose;
 
 namespace Grind.Core
 {
-	public class WebhoseClient
+    public class WebhoseClient
 	{
         private readonly WebhoseRequest webhoseRequest;
 
@@ -17,18 +13,18 @@ namespace Grind.Core
             this.webhoseRequest = new WebhoseRequest(token.CheckNotNullOrWhitespace("token"));
 		}
 
-		public Task<WebhoseResponse> Search(string searchQuery, int performanceScore, Languages language)
+		public Task<WebhoseResponse> SearchAsync(string searchQuery, int performanceScore, int responseCount, Languages language)
         {
             return Task.Run(() =>
             {
-                return webhoseRequest.getResponse(CreateUrl(searchQuery, performanceScore, language));
+                return webhoseRequest.getResponse(CreateUrl(searchQuery, performanceScore, responseCount, language));
             });
 		}
 
-        private static string CreateUrl(string query, int performanceScore, Languages language)
+        private static string CreateUrl(string query, int performanceScore, int responseCount, Languages language)
         {
-            return string.Format("{0}%20performance_score%3A%3E{1}&language={2}",
-                query.Replace(" ", "%20"), performanceScore.ToString(), language.ToString());
+            return string.Format("{0}%20performance_score%3A%3E{1}&language={2}&size={3}",
+                query.Replace(" ", "%20"), performanceScore, language.ToString(), responseCount);
         }
     }
 }
