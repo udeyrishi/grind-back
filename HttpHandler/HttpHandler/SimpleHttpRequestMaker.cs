35365e99cc6c5609e3adaf3cca1a154e73c67617
Udey Rishi <udey@ualberta.ca>
2015-09-19 01:09:13 -0400

    Added SimpleHttpRequestMaker

---
+++ HttpHandler/HttpHandler/SimpleHttpRequestMaker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpHandler
+{
+    public class SimpleHttpRequestMaker
+    {
+        public static Task<HttpResponseMessage> MakeRequestAsync(
+            HttpMethod method, 
+            string uri, 
+            string content = null, 
+            IDictionary<string, string> headers = null)
+        {
+            var request = new HttpRequestMessage(method, uri);
+
+            if (headers != null)
+            {
+                foreach(var header in headers)
+                {
+                    request.Headers.Add(header.Key, header.Value);
+                }
+            }
+            
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                request.Content = new StringContent(content);
+            }
+
+            return new HttpClient().SendAsync(request);
+        }
+    }
+}
