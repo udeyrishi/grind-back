2554230180fe476896b4f51f51c17ec9bb772500
Udey Rishi <udey@ualberta.ca>
2015-09-19 14:32:39 -0400

    Began integration

---
+++ Grind.Core/Grind.Core/INewsProvider.cs
@@ -0,0 +1,15 @@
+﻿using Grind.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind.Core
+{
+    public interface INewsProvider
+    {
+        Task<NewsLookupResult> GetNewsFromKeyword(string keyword, int performanceScore);
+        Task<NewsLookupResult> GetNewsFromUrl(string uri, int performanceScore);
+    }
+}
---
+++ Grind.Core/Grind.Core/SentimentalNewsProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grind.Core.Models;
+
+namespace Grind.Core
+{
+    public class SentimentalNewsProvider : INewsProvider
+    {
+        public Task<NewsLookupResult> GetNewsFromKeyword(string keyword, int performanceScore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsLookupResult> GetNewsFromUrl(string uri, int performanceScore)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
