de6c8efe6f8b654cd955dc18954f8255900d001d
Udey Rishi <udey@ualberta.ca>
2015-09-19 01:56:01 -0400

    Adding Utilities.Preconditions

---
+++ Utilities/Utilities/Preconditions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Preconditions
+    {
+        public static T CheckNotNull<T>(this T obj, string parameterName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(parameterName + " can't be null.", parameterName);
+            }
+
+            return obj;
+        }
+
+        public static string CheckNotNullOrWhitespace(this string str, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(parameterName + " can't be null or whitespace.", parameterName);
+            }
+
+            return str;
+        }
+
+    }
+}
--- Utilities/Utilities/Properties/AssemblyInfo.cs
+++ Utilities/Utilities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HttpHandler")]
+[assembly: AssemblyTitle("Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HttpHandler")]
+[assembly: AssemblyProduct("Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
