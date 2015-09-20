491cc99458948e21c2cc8d4530dcd0b99cad60d8
Udey Rishi <udey@ualberta.ca>
2015-09-19 22:11:28 -0400

    Bug fix: Added X-Next-Url format checking

--- Grind.Core/Grind.Core/INewsProvider.cs
+++ Grind.Core/Grind.Core/INewsProvider.cs
@@ -10,6 +10,6 @@ namespace Grind.Core
     public interface INewsProvider
     {
         Task<NewsLookupResult> GetNewsFromKeyword(string keyword, int performanceScore, int responseCount, int keywordCount, double namedEntitiesThreshold);
-        Task<NewsLookupResult> GetNewsFromUrl(string uri, int performanceScore, int keywordCount, double namedEntitiesThreshold);
+        Task<NewsLookupResult> GetNewsFromUrl(Uri uri, int performanceScore, int keywordCount, double namedEntitiesThreshold);
     }
 }
